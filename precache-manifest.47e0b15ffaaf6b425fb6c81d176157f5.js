cf7788a372d14df9d667519c2af0299d7f5c891c
Brittany Giles <giles.brittany@gmail.com>
2020-03-03 15:32:12 -0500

    Updates

---
+++ precache-manifest.47e0b15ffaaf6b425fb6c81d176157f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b7245c61910dfced83d29d98a1d5729",
+    "url": "/brittany-giles-project-5/index.html"
+  },
+  {
+    "revision": "6de98c81cbc6d900bc77",
+    "url": "/brittany-giles-project-5/static/css/main.5d673707.chunk.css"
+  },
+  {
+    "revision": "4e3a40ade1e79e7826e6",
+    "url": "/brittany-giles-project-5/static/js/2.5e48e656.chunk.js"
+  },
+  {
+    "revision": "98bcf352637c1e5a83f324253251c0f0",
+    "url": "/brittany-giles-project-5/static/js/2.5e48e656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6de98c81cbc6d900bc77",
+    "url": "/brittany-giles-project-5/static/js/main.17342992.chunk.js"
+  },
+  {
+    "revision": "3847140fc508acc39869",
+    "url": "/brittany-giles-project-5/static/js/runtime-main.4b0b6f52.js"
+  },
+  {
+    "revision": "1002c4fdf1de89e1fbc6902f6c02f3aa",
+    "url": "/brittany-giles-project-5/static/media/paperTexture.1002c4fd.png"
+  }
+]);
